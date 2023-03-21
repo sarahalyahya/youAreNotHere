c645ce7d2fccef2a2fb57739c13852b70c059b35
Sarah Al-Yahya <57350290+sarahalyahya@users.noreply.github.com>
2023-03-21 21:28:42 +0400

    Update server.js

--- server.js
+++ server.js
@@ -1,44 +1,41 @@
 var express = require('express'); 
+var bodyParser = require("body-parser");
+var path = require("path")
 var app = express();
-
-const fs = require('fs');  
+const fs = require('fs'); 
 var csvWriter = require('csv-write-stream');
 var writer = csvWriter ({sendHeaders:false}); 
-var csvFilename = "Pins.csv" 
-
-// getting data from HTML -- using http request, but that excludes lat, lng, and id 
-//how to export objectArray to node dynamically??? 
-
-console.log(require('./public/sketch.js'));
-if (!fs.existsSync(csvFilename)) {
-    writer = csvWriter({sendHeaders: false});
-    writer.pipe(fs.createWriteStream(csvFilename));
-    writer.write({
-      header1: 'lat',
-      header2: 'lng',
-      header3: 'name',
-      header4: 'desc',
-      header5: 'id'
-    });
-    writer.end();
-  } 
-
-
-
- writer = csvWriter({sendHeaders:false});
- writer.pipe(fs.createWriteStream(csvFilename,{flags:'a'}));
- writer.write({
-    header1: '34.5575',
-    header2: '35.67876543',
-    header3: 'home',
-    header4: 'homehomehome',
-    header5:'12345678'
- });
- writer.end(); 
-
+const port = 3000;
+app.use(bodyParser.urlencoded({extended : false}));
 app.use(express.static('public'));
 
 
 
+app.listen(port, ()=>console.log("server good"));
 
-app.listen(3000); 
+app.get("/", function(req,res){
+    res.sendFile(path.join(__dirname,"/index.html"));
+});
+
+app.post("/ajax_response",function(req,res){
+    console.log(req.body.latitude);
+    if (!fs.existsSync("./public/Pins.csv"))
+        writer = csvWriter({ headers: ["latitude","longitude","locName","locDesc","id"]});
+    else
+        writer = csvWriter({sendHeaders: false});
+    writer.pipe(fs.createWriteStream("./public/Pins.csv", {flags: 'a'}));
+    writer.write({
+        header1:req.body.latitude,
+        header2:req.body.longitude,
+        header3:req.body.locName,
+        header4:req.body.locDesc,
+        header5:req.body.id,
+    });
+    writer.end();
+    // res.json(data);
+    // console.log(data);
+
+})
+
+
+

3589728c6d10db312937abcfa8e05f69d5931842
Sarah Al-Yahya <57350290+sarahalyahya@users.noreply.github.com>
2023-03-25 00:54:48 +0400

    update server.js with edited headers

--- server.js
+++ server.js
@@ -20,24 +20,22 @@ app.get("/", function(req,res){
 app.post("/ajax_response",function(req,res){
     console.log(req.body.latitude);
     if (!fs.existsSync("./public/Pins.csv"))
-        writer = csvWriter({ headers: ["x","y","latitude","longitude","locName","locDesc","id"]});
+        writer = csvWriter({ headers: ["x","y","longitude","latitude","locName","locDesc","zoom","id"]});
     else
         writer = csvWriter({sendHeaders: false});
     writer.pipe(fs.createWriteStream("./public/Pins.csv", {flags: 'a'}));
     writer.write({
         header1:req.body.x,
         header2:req.body.y,
-        header3:req.body.latitude,
-        header4:req.body.longitude,
+        header3:req.body.longitude,
+        header4:req.body.latitude,
         header5:req.body.locName,
         header6:req.body.locDesc,
-        header7:req.body.id,
+        header7:req.body.zoom,
+        header8:req.body.id,
     });
     writer.end();
     // res.json(data);
     // console.log(data);
 
 })
-
-
-

34feabc3464cb19ad0e002c5a474fcf02df7c927
Sarah Al-Yahya <57350290+sarahalyahya@users.noreply.github.com>
2023-04-07 01:38:14 +0400

    fix grid display

--- public/sketch.js
+++ public/sketch.js
@@ -1,6 +1,4 @@
-//TO-DO:
-// > FIX GRID DISPLAY
-// > FIGURE OUT PIN COLORS 
+
 
 //Pin Object Array
 let pins = [];
@@ -8,10 +6,19 @@ let pins = [];
 let allPinsTable;
 //Lat lng based on click
 let position;
-//Image Ratio to maintain correctly scaled image
-let imageRatio = 0.729;
-let pinHeight = 35;
-let pinWidth = pinHeight * imageRatio;
+
+let numColumns = 35; 
+let horzMargin = 10; 
+let vertMargin = 10;
+let vertOffset = 5; 
+let horzOffset = horzMargin/2; 
+let xMult;
+let pinWidth;
+let pinHeight; 
+let redPin;
+let darkRedPin;
+let greyPin; 
+let whitePin; 
 
 const key =
   "pk.eyJ1Ijoic2FyYWhhbHlhaHlhIiwiYSI6ImNsY3JjZGo3eDBlbmozeG1zbWV5enF3NHEifQ.riUptbqKaET6RE6XJ8usvg";
@@ -20,7 +27,7 @@ const key =
 const options = {
   lng: 0,
   lat: 0,
-  zoom: 1.6,
+  zoom: 1.8,
   style: "mapbox://styles/sarahalyahya/clddbjxfo000901lasb1bl3ur",
   pitch: 0,
 };
@@ -41,11 +48,12 @@ function preload() {
     // Callback:
     function (table) {
       loaded = true;
-      console.log(loaded);
     }
   );
   redPin = loadImage("redNavFull.png");
+  darkRedPin = loadImage("darkRedNavFull.png");
   whitePin = loadImage("whiteNavFull.png");
+  greyPin = loadImage("greyNavFull.png");
 }
 
 
@@ -53,18 +61,33 @@ function preload() {
 function setup() {
   //Convert CSV to Array
   pins=allPinsTable.getArray();
-  console.log("after loading;",pins);
   canvas = createCanvas(windowWidth, windowHeight);
+  pinWidth = (width-(horzMargin*numColumns))/(numColumns); 
+  pinHeight = pinWidth*(redPin.height/redPin.width); 
+  xMult = width/(numColumns); 
 
+  let index = pins.length; 
+  console.log("before loop:", pins);
+  
+
+  for(let i =0; i<index; i++){
+    let x = i%numColumns;
+    let y = floor(i/numColumns);
+    pins[i][0] = (x*xMult)+horzOffset; 
+    pins[i][1] = y*(pinHeight+vertMargin)+vertOffset; 
+  }
+  console.log("after loop:", pins);
+  
   myMap = mappa.tileMap(options);
   myMap.overlay(canvas);
+  //background(3,13,28);
+  
 
  }
 
 // Grabbing HTML elements
 //Add Pin Modal
 const addPinModal = document.querySelector(".addPinModal");
-const displayLocationModal = document.querySelector(".locationModal");
 const cancelLocationAdd = document.querySelector(".form__cancel");
 const createLocation = document.querySelector(".form__add");
 const closePinModalBtn = document.querySelector(".closePinModal");
@@ -77,6 +100,7 @@ const showInstructionsBtn = document.getElementById("add-a-Pin");
 const closeInstructionsModalBtn = document.querySelector(".closeInstructionsModal");
 
 //Display Location Modal 
+const displayLocationModal = document.querySelector(".locationModal");
 const closeLocationModalBtn = document.querySelector(".closeLocationModal");
 const displayedLocationCoordinates = document.getElementById("coord-placeholder-filled");
 const displayedLocationName = document.getElementById("loc-name-filled");
@@ -88,6 +112,7 @@ const displayedLocationDescription = document.getElementById("loc-description-fi
 
 //Gets latitude and longitude based on mouse click location and injects it into html + opens add pin modal
 function addLocationOnMap() {
+      displayLocationModal.classList.add("hidden");
       position = myMap.pixelToLatLng(mouseX, mouseY);
       document.getElementById("placeholder").innerHTML = `${position.lng}, ${position.lat}`;
       togglePinModal();
@@ -99,15 +124,15 @@ function createPin() {
   //const positionDisplayed = document.getElementById("placeholder").innerHTML.split(","); *i THINK i don't need this*
   const locationDescriptionEdited = locationDescription.value.trim();
   const locationNameEdited = locationName.value.trim();
-
+  
   //User must fill in at least one of the inputs 
   if (locationDescriptionEdited.length == 0 && locationNameEdited.length == 0) {
     alert("You must enter either a location name or description");
   } else {
-    //Create pin object
+    
     let currentPin = new Pin(
-      0,
-      0,
+     0,
+     0,
       position.lng,
       position.lat,
       locationNameEdited,
@@ -134,18 +159,35 @@ function displayPinsLineup(){
     }else{
       image(whitePin, pin[0], pin[1], pinWidth, pinHeight);
     }
+
+    if (
+      mouseX > pin[0] &&
+      mouseX < pin[0] + pinWidth &&
+      mouseY > pin[1] &&
+      mouseY < pin[1] + pinHeight
+    ) {
+
+
+      if(pin[7] == 1){
+      image(darkRedPin, pin[0], pin[1], pinWidth, pinHeight);
+    }else{
+      image(greyPin, pin[0], pin[1], pinWidth, pinHeight);
+    }
+
+
+    }
+
     
   });
 
 }
 
 
-
-
 //Modal Functionalities
 
 //Displays added location modal 
 function showLocationModal(pin) {
+  addPinModal.classList.add("hidden");
   //takes object values and displays them in the HTML element 
   displayedLocationCoordinates.innerHTML = `${pin[2]},${pin[3]}`;
   displayedLocationName.innerHTML = pin[4];
@@ -180,13 +222,20 @@ closePinModalBtn.addEventListener("click", (e) => {
 //Shows instruction modal
 showInstructionsBtn.addEventListener("click", (e) => {
   e.preventDefault();
+  console.log("here");
+  displayLocationModal.classList.add("hidden");
+  addPinModal.classList.add("hidden");
+  console.log("and here");
   instructionsModal.classList.remove("hidden");
+  
+  
 });
 
 //Closes instructions modal
 closeInstructionsModalBtn.addEventListener("click", (e) => {
   e.preventDefault();
   instructionsModal.classList.add("hidden");
+  
 });
 
 //Closes location modal
@@ -197,7 +246,8 @@ closeLocationModalBtn.addEventListener("click", (e) => {
        0,0
         
       ],
-    zoom: 1.6
+    zoom: 1.6,
+    duration: 11000
   });
   displayLocationModal.classList.add("hidden");
 
@@ -211,7 +261,8 @@ cancelLocationAdd.addEventListener("click", (e) => {
        0,0
         
       ],
-    zoom: 1.6
+    zoom: 1.6,
+    duration: 9000
   });
 });
 
@@ -229,7 +280,15 @@ createLocation.addEventListener("click", (e) => {
   forPostReq(pin);
   //Because i've converted the CSV table to an array, i must convert the pin object to a pin array before i push it
   const myArray = Object.values(pin);
-  console.log("pin Array:",myArray);
+
+  let x = currentPinsArrayLength%numColumns;
+  let y = floor(currentPinsArrayLength/numColumns); 
+  myArray[0] = (x*xMult)+horzOffset;
+  myArray[1] = y*(pinHeight+vertMargin)+vertOffset; 
+
+  
+  
+
   pins.push(myArray);
 
   if (pins.length > currentPinsArrayLength) {
@@ -240,7 +299,8 @@ createLocation.addEventListener("click", (e) => {
        0,0
         
       ],
-    zoom: 1.6
+    zoom: 1.6,
+    duration: 9000
   });
 });
 
@@ -256,7 +316,10 @@ class Pin {
     this.zoom = _zoom;
     this.id = id;
   }
-}
+  
+
+  }
+
 
 
 
@@ -266,36 +329,7 @@ function draw() {
   canvas.doubleClicked(addLocationOnMap);
 
   
-  //OVERFLOW NEEDS TO BE FIGURED OUT
   
-    //Calculating number of row and cols based on canvas and pin size 
-    var numRows = floor(canvas.height / pinHeight);
-    var numCols = floor(canvas.width / pinWidth)-25;
-
-    // Calculate the horizontal and vertical spacing between elements
-    var xSpacing = (canvas.width / numCols)+5;
-    var ySpacing = (canvas.height / numRows)+5;
-   
-    // Loop through pins array and give them the x,y values needed later for displaying the pins 
-    for (var i = 0; i < pins.length; i++) {
-
-      // Calculate the row and column of the current element
-      //based on columns because "number of columns" is what makes up a row
-      var row = floor(i / numCols);
-
-      //Since i/numCols is the row, the remainder gives us the specific cell of the row the object is in (so the column)
-      var col = i % numCols;
-
-      // Calculate the x and y position of the pin and center it with taking spacing into account 
-      var x = col * xSpacing + (xSpacing - (pinWidth)) / 2;
-      var y = row * ySpacing + (ySpacing - pinHeight) / 2;
-
-      //adding the x,y
-      pins[i][0] = x;
-      pins[i][1] = y;
-      
-    }
-
   displayPinsLineup();
   
 }
@@ -317,7 +351,8 @@ function mousePressed() {
            pin[2],pin[3]
             
           ],
-        zoom: pin[6]
+        zoom: pin[6],
+        duration: 9000
       });
       
      
@@ -327,4 +362,7 @@ function mousePressed() {
     } 
   });
 }
+
+
+
 
